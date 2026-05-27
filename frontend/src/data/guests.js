4b8d817ae2915277a6cb3792e0e276fa5d1db6ea
Lucas-MAP <luckdev.98@gmail.com>
2026-05-27 17:53:14 -0300

    ajuste na lista de convidados

--- frontend/src/data/guests.js
+++ frontend/src/data/guests.js
@@ -1,95 +1,116 @@
 export const guests = [
-  // FAMÍLIA DO NOIVO
-  { name: "Tadeu José", maxGuests: 1 },
-  { name: "Telma Alves", maxGuests: 1 },
-  { name: "Mariana Alves", maxGuests: 1 },
+  // FAMÍLIA 1
+  { name: "Tadeu Alves", maxGuests: 1 },
+  { name: "Telma Alves", maxGuests: 3 },
+  { name: "Mariana Alves", maxGuests: 3 },
   { name: "Rodolfo Alves", maxGuests: 1 },
   { name: "Rafael Alves", maxGuests: 1 },
-  { name: "Thayana Alves", maxGuests: 1 },
-  { name: "Charles", maxGuests: 1 },
+  { name: "Thayana Alves", maxGuests: 3 },
+  { name: "Charles marido da thayana", maxGuests: 3 },
   { name: "Rita Alves", maxGuests: 1 },
-  { name: "Isabel Chagas", maxGuests: 1 },
-  { name: "Taisa Pinheiro", maxGuests: 1 },
-  { name: "Joana Chagas Pinheiro", maxGuests: 1 },
+
+  // FAMÍLIA 2
+  { name: "Isabel Chagas", maxGuests: 3 },
+  { name: "Taisa Pinheiro", maxGuests: 3 },
+  { name: "Joana Chagas Pinheiro", maxGuests: 3 },
   { name: "Augusto Belchior", maxGuests: 1 },
   { name: "Antonia Chagas Pinheiro", maxGuests: 1 },
   { name: "Benedita Chagas Pinheiro", maxGuests: 1 },
   { name: "Rodão Chagas Pinheiro", maxGuests: 4 },
 
-  // PADRINHOS
+  // FAMÍLIA 3
   { name: "William Gabriel Souza Quayson", maxGuests: 3, isGodfather: true },
+  { name: "Fernanda Cristina Souza Lobato da Cunha", maxGuests: 3 },
   { name: "Adria Kallfman", maxGuests: 3, isGodmother: true },
+  { name: "Glaucia Lobato", maxGuests: 1 },
+
+  // FAMÍLIA 4
   { name: "Dalila Maria", maxGuests: 2, isGodmother: true },
   { name: "Thiago Alves", maxGuests: 2, isGodfather: true },
-  { name: "Thalya Menezes", maxGuests: 2, isGodmother: true },
-  { name: "Henrique Odilon", maxGuests: 2, isGodfather: true },
-  { name: "Nathalia Almeida", maxGuests: 2, isGodmother: true },
-  { name: "Victor Alexandre", maxGuests: 2, isGodfather: true },
-  { name: "Aldo Luiz", maxGuests: 2, isGodfather: true },
-  { name: "Arnold Thiago Luz Campos", maxGuests: 2, isGodfather: true },
-  { name: "Bruno Moraes de Oliveira", maxGuests: 4, isGodfather: true },
-  { name: "Ana Clara", maxGuests: 2, isGodmother: true },
-  { name: "Lohanne Sarmento", maxGuests: 2, isGodmother: true },
-  { name: "Elisabeth Dias", maxGuests: 2, isGodmother: true },
-  { name: "um e dois", maxGuests: 4, isGodfather: true },
-  { name: "dois e um", maxGuests: 2, isGodmother: true },
-
-  
-
-  // FAMÍLIA DO NOIVO - OUTROS
-  { name: "Mãe do Will", maxGuests: 1 },
-  { name: "Madalena Maria", maxGuests: 1 },
+  { name: "Madalena Maria", maxGuests: 2 },
   { name: "Dulce Maria", maxGuests: 2 },
-  { name: "Hanny", maxGuests: 1 },
-  { name: "Mateus Almeida", maxGuests: 1 },
-  { name: "Marcelly", maxGuests: 1 },
-  { name: "Nazaré do Socorro", maxGuests: 2 },
-  { name: "Jorge Garcia", maxGuests: 2 },
-  { name: "Angélica Luz", maxGuests: 1 },
-  { name: "Aldenair Luz", maxGuests: 1 },
-  { name: "Wallace Luz", maxGuests: 1 },
 
-  // PASTORES / FAMÍLIA 7-8
-  { name: "Pastor Marcio", maxGuests: 1 },
-  { name: "Alessandra", maxGuests: 1 },
-  { name: "Pastor Carlos", maxGuests: 3 },
-  { name: "Tia Emilia", maxGuests: 3 },
+  // FAMÍLIA 5
+  { name: "Thalya Soares Menezes", maxGuests: 3, isGodmother: true },
+  { name: "Henrique Menezes", maxGuests: 3, isGodfather: true },
+  { name: "Haniely Soares Menezes", maxGuests: 3 },
 
-  // FAMÍLIA ALVES
+  // FAMÍLIA 6
+  { name: "Nathalia Almeida", maxGuests: 1, isGodmother: true },
+  { name: "Mateus Almeida", maxGuests: 3 },
+  { name: "Amanda Silva", maxGuests: 3 },
+  { name: "Victor Alexandre", maxGuests: 1, isGodfather: true },
+  { name: "Nazaré do Socorro", maxGuests: 3 },
+  { name: "Jorge Garcia", maxGuests: 3 },
+
+  // FAMÍLIA 7
+  { name: "Wendell Marcio Silva", maxGuests: 2 },
+  { name: "Alessandra Nunes", maxGuests: 2 },
+
+  // FAMÍLIA 8
+  { name: "Carlos Silva", maxGuests: 3 },
+  { name: "Emilia", maxGuests: 3 },
+
+  // FAMÍLIA 9
   { name: "Aldo Alves", maxGuests: 3 },
   { name: "Ana Alves", maxGuests: 3 },
   { name: "Alan Alves", maxGuests: 1 },
+  { name: "Aldo Luiz", maxGuests: 1, isGodfather: true },
 
-  // FAMÍLIA DA NOIVA
+  // FAMÍLIA 10
   { name: "Sebastiana Silva", maxGuests: 3 },
   { name: "Manuel Lima", maxGuests: 3 },
   { name: "Marlene Silva", maxGuests: 3 },
   { name: "Josenildo Ferreira", maxGuests: 3 },
-  { name: "Joelison Ferreira", maxGuests: 1 },
-  { name: "Julyene Ferreira", maxGuests: 1 },
-  { name: "Antonia Jamyce", maxGuests: 1 },
+  { name: "Joelison Ferreira", maxGuests: 3 },
+  { name: "Julyene Ferreira", maxGuests: 3 },
+  { name: "Antonia Jamylce", maxGuests: 1 },
   { name: "Josenilson Ferreira", maxGuests: 1 },
   { name: "Dina Ferreira", maxGuests: 1 },
   { name: "Maria de Fátima", maxGuests: 1 },
 
   // FAMÍLIA 11
-  { name: "Adriana Almeida", maxGuests: 1 },
-  { name: "Francislei Fernandes", maxGuests: 1 },
-  { name: "Luis Henrique", maxGuests: 1 },
-  { name: "Lívia Beatriz", maxGuests: 1 },
+  { name: "Adriana Almeida", maxGuests: 3 },
+  { name: "Francislei Fernandes", maxGuests: 3 },
+  { name: "Luis Henrique", maxGuests: 3 },
+  { name: "Lívia Beatriz", maxGuests: 3 },
   { name: "Flaviane Fernandes", maxGuests: 1 },
+  { name: "Jailson", maxGuests: 1 },
   { name: "Kauane Fernandes", maxGuests: 1 },
 
   // FAMÍLIA 12
-  { name: "Diego RPG", maxGuests: 1 },
-  { name: "Rodrigo Lima", maxGuests: 4 },
-  { name: "Ranyelle", maxGuests: 1 },
+  { name: "Diego Viana Delfino", maxGuests: 3 },
+  { name: "Meires Karolina Favacho de Almeida", maxGuests: 3 },
 
-  // PASTOR OMAR
-  { name: "Omar Pastor", maxGuests: 1 },
-  { name: "Andreta de Paula", maxGuests: 1 },
+  // FAMÍLIA 13
+  { name: "Omar Martins", maxGuests: 2 },
+  { name: "Andreta de Paula", maxGuests: 2 },
+  { name: "Bruno Moraes de Oliveira", maxGuests: 4, isGodfather: true },
+  { name: "Ana Clara", maxGuests: 4, isGodmother: true },
+  { name: "Lorena Moraes de Oliveira", maxGuests: 4 },
+  { name: "Mara Moraes de Oliveira", maxGuests: 4 },
+  { name: "Cláudio Canêdo de Oliveira", maxGuests: 4 },
 
-  // OUTROS
-  { name: "Osorio Passos Reis", maxGuests: 1 },
-  { name: "Elis Regina Cardoso Felipe", maxGuests: 1 },
+  // FAMÍLIA 14
+  { name: "Arnold Thiago", maxGuests: 1, isGodfather: true },
+  { name: "Angélica Luz", maxGuests: 1 },
+  { name: "Aldenair Luz", maxGuests: 1 },
+  { name: "Wallace Luz", maxGuests: 1 },
+  { name: "Osorio Passos Reis", maxGuests: 2 },
+  { name: "Elis Regina Cardoso Felipe", maxGuests: 2 },
+
+  // FAMÍLIA 15
+  { name: "Ellane Nascimento", maxGuests: 2 },
+  { name: "Edson Mendes", maxGuests: 2 },
+
+  // FAMÍLIA 16
+  { name: "Cláudio Oliveira", maxGuests: 2 },
+  { name: "Jaqueline Oliveira", maxGuests: 2 },
+
+  // INDIVIDUAIS
+  { name: "Lohanne Sarmento", maxGuests: 1, isGodmother: true },
+  { name: "Elisabeth Dias", maxGuests: 1, isGodmother: true },
+
+  // EXTRA
+  { name: "Nicole", maxGuests: 1 },
 ];
